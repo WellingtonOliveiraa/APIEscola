519c1037f70fd8a47a2fda2c28f43424f5ffe868
Wellington Oliveira <wellington.oliveira@s4e.com.br>
2026-06-19 00:06:11 -0300

    OK

--- Escola/Program.cs
+++ Escola/Program.cs
@@ -19,12 +19,8 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Escola"));
-}
+app.MapOpenApi();
+app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Escola"));
 
 app.UseHttpsRedirection();
 
